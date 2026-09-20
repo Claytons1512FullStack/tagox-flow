64d5135d07ca7d5180ffda7bc596453b0ae4dacc
Clayton Sales <claytonsalesfullstack@gmail.com>
2026-09-20 16:27:16 -0300

    test: configura perfil test no AuthServiceTest

--- backend/src/test/java/com/tagox/flow/AuthServiceTest.java
+++ backend/src/test/java/com/tagox/flow/AuthServiceTest.java
@@ -18,12 +18,14 @@ import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class AuthServiceTest {
 
